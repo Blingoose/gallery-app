16ca189de9c479a18f9f738a789b371c99706ecf
Andre Katov <andy.katov@icloud.com>
2022-12-01 01:33:13 +0200

    minor change

--- src/router/Router.js
+++ src/router/Router.js
@@ -3,13 +3,15 @@ import { Routes, Route } from "react-router-dom";
 import About from "../pages/About";
 import Header from "../components/Header";
 import App from "../pages/App";
+import Gallery from "../pages/Gallery";
 
 const Router = () => {
   return (
     <div>
       <Header />
       <Routes>
-        <Route path="/gallery" element={<App />} />
+        <Route path="/" element={<App />} />
+        <Route path="/gallery" element={<Gallery />} />
         <Route path="/about" element={<About />} />
       </Routes>
     </div>
