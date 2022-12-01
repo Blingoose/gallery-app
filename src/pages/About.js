fd6d61765ca04a5674228b459abe635cb958a093
Andre Katov <andy.katov@icloud.com>
2022-12-01 02:00:34 +0200

    minor change

--- src/pages/About.js
+++ src/pages/About.js
@@ -4,7 +4,27 @@ import "../styles/About.css";
 const Home = () => {
   return (
     <div className="about">
-      <div className="center"> A picture is worth a thousand words </div>
+      {/* <div className="center">
+        <div className="left">
+          <h1 className="h2">
+            Well, that a sad about page, don't you think so? a little addition
+            of color would definitely improve that awful situation, right?
+            <br />
+          </h1>
+        </div>
+        <div className="right">
+
+        <h2 className="h1">
+          And that's what we're all about! Image-in is the most awesome image
+          gallery that you'll ever need.
+          <br />
+          <span className="quote">"A picture is worth a thousand words"</span>
+          <br />
+          Now, what about a thousand pictures.. or.. maybe even more! It's all
+          up to you, our beloved users.
+        </h2>
+        </div>
+      </div> */}
     </div>
   );
 };
